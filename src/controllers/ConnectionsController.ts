d5afb4389a54f55bfc56f5ea02b9c8f90f93de66
Diego Goulart <diegogoulart.aws@gmail.com>
2020-08-08 13:49:46 -0300

    feat: Classes, Schedule and Connections

---
+++ src/controllers/ConnectionsController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import db from '../database/connection';
+import convertHourToMinutes from '../utils/convertHourToMinutes';
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+export default class ConnectionsController {
+  async index(request: Request, response: Response): Promise<Response> {
+    const totalConnections = await db('connections').count('* as total');
+
+    return response.json(totalConnections[0]);
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { user_id } = request.body;
+
+    const trx = await db.transaction();
+
+    try {
+      await trx('connections').insert({
+        user_id,
+      });
+
+      await trx.commit();
+
+      return response.status(201).send();
+    } catch (error) {
+      await trx.rollback();
+
+      return response.status(400).json({
+        error: 'Unexpected error while creating new connection',
+      });
+    }
+  }
+}
